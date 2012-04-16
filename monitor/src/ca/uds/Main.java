8b8983f9c8ec23be4b04ab8fcd548dd2b4ebfbd2
girc9301 <girc9301@7d357df0-83eb-11e1-ba90-f758bd4867e8>
2012-04-16 15:16:35 +0000

    git-svn-id: svn+ssh://svn.gel.usherbrooke.ca/grp/svn/lannion12/trunk@3 7d357df0-83eb-11e1-ba90-f758bd4867e8

---
+++ monitor/src/ca/uds/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ca.uds;
+
+/**
+ * @author Charlotte
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("allo");
+		
+		
+		System.out.println("bye");
+	}
+
+}
