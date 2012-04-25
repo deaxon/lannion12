9c36e849cad45fefc1cb6b0e16397688e55f7f8f
gonzrubi <gonzrubi@7d357df0-83eb-11e1-ba90-f758bd4867e8>
2012-04-25 19:48:39 +0000

    git-svn-id: svn+ssh://svn.gel.usherbrooke.ca/grp/svn/lannion12/trunk@53 7d357df0-83eb-11e1-ba90-f758bd4867e8

--- monitor/src/ca/uds/LogConnexion.java
+++ monitor/src/ca/uds/LogConnexion.java
@@ -1,82 +1,82 @@
-/**
- * 
- */
-package ca.uds;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import urlConnection.UrlConnection;
-
-/**
- * @author Charlotte
- * 
- */
-public class LogConnexion {
-	// HttpClient client = new HttpClient();
-	UrlConnection myURLConnection = new UrlConnection();
-	URL url = null;
-
-	public boolean sendLog(String url, String login, String passwd) {
-		boolean b = false;
-		boolean connState = myURLConnection.urlConnect(url);
-		HttpClient client = new HttpClient();
-		PostMethod method = new PostMethod(url);
-		method.addParameter("ndeveloper[cip]", login);
-		method.addParameter("ndeveloper[password]", passwd);
-		BufferedReader br = null;
-		try {
-			// int returnCode = client.executeMethod(method);
-			// System.out.println("La reponse de executeMethod est : " +
-			// returnCode);
-			br = new BufferedReader(new InputStreamReader(
-					method.getResponseBodyAsStream()));
-			String readLine;
-
-			while (((readLine = br.readLine()) != null)) {
-				System.out.println(readLine);
-			}
-			b = true;
-		} catch (Exception e) {
-			System.err.println(e);
-		} finally {
-			method.releaseConnection();
-			if (br != null) {
-				try {
-					br.close();
-
-				} catch (Exception e) {
-				}
-			}
-
-		}
-
-		// if (connState == true) {
-		// try {
-		// URL u = new URL(url);
-		// String charset = "UTF-8";
-		// URLConnection conn = u.openConnection();
-		// conn.setDoOutput(true);
-		//
-		//
-		//
-		// b = true;
-		//
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
-		// }
-		return b;
-	}
-}
+///**
+// * 
+// */
+//package ca.uds;
+//
+//import java.io.IOException;
+//import java.io.OutputStream;
+//
+//import java.net.URL;
+//import java.net.URLConnection;
+//import java.net.URLEncoder;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//import org.apache.commons.httpclient.methods.PostMethod;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.impl.client.DefaultHttpClient;
+//
+////import urlConnection.UrlConnection;
+//
+///**
+// * @author Charlotte
+// * 
+// */
+//public class LogConnexion {
+//	// HttpClient client = new HttpClient();
+//	UrlConnection myURLConnection = new UrlConnection();
+//	URL url = null;
+//
+//	public boolean sendLog(String url, String login, String passwd) {
+//		boolean b = false;
+//		boolean connState = myURLConnection.urlConnect(url);
+//		HttpClient client = new HttpClient();
+//		PostMethod method = new PostMethod(url);
+//		method.addParameter("ndeveloper[cip]", login);
+//		method.addParameter("ndeveloper[password]", passwd);
+//		BufferedReader br = null;
+//		try {
+//			// int returnCode = client.executeMethod(method);
+//			// System.out.println("La reponse de executeMethod est : " +
+//			// returnCode);
+//			br = new BufferedReader(new InputStreamReader(
+//					method.getResponseBodyAsStream()));
+//			String readLine;
+//
+//			while (((readLine = br.readLine()) != null)) {
+//				System.out.println(readLine);
+//			}
+//			b = true;
+//		} catch (Exception e) {
+//			System.err.println(e);
+//		} finally {
+//			method.releaseConnection();
+//			if (br != null) {
+//				try {
+//					br.close();
+//
+//				} catch (Exception e) {
+//				}
+//			}
+//
+//		}
+//
+//		// if (connState == true) {
+//		// try {
+//		// URL u = new URL(url);
+//		// String charset = "UTF-8";
+//		// URLConnection conn = u.openConnection();
+//		// conn.setDoOutput(true);
+//		//
+//		//
+//		//
+//		// b = true;
+//		//
+//		// } catch (Exception e) {
+//		// e.printStackTrace();
+//		// }
+//		// }
+//		return b;
+//	}
+//}
