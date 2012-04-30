8639add23f13b573fe508c8c9b8a2ff01e89a927
gukb2401 <gukb2401@7d357df0-83eb-11e1-ba90-f758bd4867e8>
2012-04-30 16:26:03 +0000

    git-svn-id: svn+ssh://svn.gel.usherbrooke.ca/grp/svn/lannion12/trunk@84 7d357df0-83eb-11e1-ba90-f758bd4867e8

--- MiniSite/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MiniSite::Application.config.secret_token = 'bb1717b91281076ec735082b71c01a8ef30e22d4b58e4a1598b85cac533e364a09a1d724dae42364260c673a540860f211b9effe32481a0919a61c39904d2b02'
