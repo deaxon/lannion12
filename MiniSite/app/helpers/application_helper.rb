2c2adc43d03e3dac494400bdb7b40a698efa7324
crea9001 <crea9001@7d357df0-83eb-11e1-ba90-f758bd4867e8>
2012-05-04 13:17:00 +0000

    git-svn-id: svn+ssh://svn.gel.usherbrooke.ca/grp/svn/lannion12/trunk@98 7d357df0-83eb-11e1-ba90-f758bd4867e8

--- MiniSite/app/helpers/application_helper.rb
+++ MiniSite/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def selected_class (but)
+    case but
+      when 'home'
+        return "selected" if controller.controller_name == "home" and  controller.action_name == "index"
+      when 'projects'
+        return "selected" if controller.controller_name == "projects" and  controller.action_name == "project"
+      when "contacts"
+        return "selected" if controller.controller_name == "contacts" and  controller.action_name == "contact"
+      else
+        return "selected" if controller.controller_name == but
+    end
+  end
 end
